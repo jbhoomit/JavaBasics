a77f152704d1efe5b67448065f7fdfeedcfccec6
bhoomit jain <bhoonid@gmail.com>
2026-06-09 18:49:33 +0530

    day3 IncrementDcrementoperators

---
+++ src/IncrementDcrementoprators.java
@@ -0,0 +1,13 @@
+public class IncrementDcrementoprators {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println("Initial valie:"+a);
+
+        a++;//increment by 1
+        System.out.println("after a++:" +a);
+
+        a--;//dcrement by 1
+        System.out.println("after a--:" +a);
+    }
+}
