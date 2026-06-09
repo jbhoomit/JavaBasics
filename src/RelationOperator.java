823530377a5ba7314bdecb8188f45717e2b7bf35
bhoomit jain <bhoonid@gmail.com>
2026-06-09 18:18:55 +0530

    day 3 relationOprators

---
+++ src/RelationOperator.java
@@ -0,0 +1,13 @@
+public class RelationOperator {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 30;
+
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+    }
+}
