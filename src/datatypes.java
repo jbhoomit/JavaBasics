dd8570bb72e10b78657017b3ecb8540374bcc2fa
bhoomit jain <bhoonid@gmail.com>
2026-06-09 17:45:03 +0530

    day 1 data types

---
+++ src/datatypes.java
@@ -0,0 +1,41 @@
+  //datatypes byte //
+public class datatypes{
+      public static void main(String[] args) {
+
+          //Byte Example
+          byte age =22;
+          System.out.println(age);
+
+
+         // short example
+          short salary = 3000;
+        System.out.println(salary);
+
+        // int example
+          int Student =20;
+          System.out.println(Student);
+
+          //long example
+          long mobileNumber=7297875390l;
+          System.out.println(mobileNumber);
+
+          //float example
+          float price =99.99f;
+          System.out.println(price);
+
+
+          //double example
+          double pi = -4.93247308;
+          System.out.println(pi);
+
+          //char example
+          char grade ='A';
+          System.out.println(grade);
+
+          //boolean example
+          boolean myAgeIs22 =true;
+          System.out.println(myAgeIs22);
+       }
+}
+
+
