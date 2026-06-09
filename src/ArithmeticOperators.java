b3c56882059cdbaea10e55406976963a25c050ce
bhoomit jain <bhoonid@gmail.com>
2026-06-09 18:09:08 +0530

    day3 Arithmetic oprators

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 30;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
