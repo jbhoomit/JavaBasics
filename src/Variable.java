6430ba08dc18a20daba036a2c932d9507711cb2a
bhoomit jain <bhoonid@gmail.com>
2026-06-09 19:00:05 +0530

    day1 variable

---
+++ src/Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[]args) {
+        int age = 22;
+        String name = "Bhumit";
+        System.out.println("Name:" +name);
+        System.out.println("Age:" +age);
+    }
+}
