89cb4cdfb1e5024254bb9a449317e68634a3a80b
bhoomit jain <bhoonid@gmail.com>
2026-06-09 18:31:44 +0530

    day3 LogicalOperators

---
+++ src/Logicaloperators.java
@@ -0,0 +1,12 @@
+public class Logicaloperators {
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 20;
+
+            System.out.println(a < b && a > 5);   // AND
+            System.out.println(a > b || a < 15);  // OR
+            System.out.println(!(a > b));         // NOT
+        }
+
+}
+
