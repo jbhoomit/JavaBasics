e4decaf4af862c421d8f659bb7973b44e12e103d
bhoomit jain <bhoonid@gmail.com>
2026-06-10 10:52:48 +0530

    day4 IfStatement

---
+++ src/IfStatement.java
@@ -0,0 +1,9 @@
+public class IfStatement {
+    public static void main(String[]args) {
+        int age = 22;
+        if(age >= 18){
+            System.out.println("you are eligible for vote");
+        }
+
+    }
+}
