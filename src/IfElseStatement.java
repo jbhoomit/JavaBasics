c231adcf6b75212e6c439a64bd94a45bfdccebf5
bhoomit jain <bhoonid@gmail.com>
2026-06-10 11:00:49 +0530

    day4 IfElseStatement

---
+++ src/IfElseStatement.java
@@ -0,0 +1,10 @@
+public class IfElseStatement {
+    public static void main(String[]args) {
+        int marks =85;
+        if (marks >= 35) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
+    }
+}
