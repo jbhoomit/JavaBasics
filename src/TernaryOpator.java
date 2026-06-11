bd8ab838d4733c32a3c5e72980928e9d8f6e5451
bhoomit jain <bhoonid@gmail.com>
2026-06-11 16:01:13 +0530

    day3 TernaryOperator

---
+++ src/TernaryOpator.java
@@ -0,0 +1,8 @@
+public class TernaryOpator {
+    public static void main(String[]args){
+        int age =22;
+        String result =(age >= 18)?"Young" :"child";
+
+        System.out.println(result);
+    }
+}
