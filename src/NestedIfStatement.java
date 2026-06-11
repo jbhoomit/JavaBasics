ed4d7e0163d663ccb7fc0ca275302be6e417982e
bhoomit jain <bhoonid@gmail.com>
2026-06-11 11:35:45 +0530

    day4 NestedIf statement

---
+++ src/NestedIfStatement.java
@@ -0,0 +1,11 @@
+public class NestedIfStatement {
+    public static void main(String[]args) {
+        int age = 22;
+        boolean hasvoterid = true;
+        if (age>=18){
+         if(hasvoterid){
+             System.out.println("You can vote");
+         }
+        }
+    }
+}
