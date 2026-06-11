1f469cd84ce3c523069939296e11c1357e66a06d
bhoomit jain <bhoonid@gmail.com>
2026-06-11 13:20:50 +0530

    day5 DoWhileLoop

---
+++ src/DoWhileLoop/Java.java
@@ -0,0 +1,12 @@
+package DoWhileLoop;
+
+public class Java {
+    public static void main(String[]args) {
+        int n = 1;
+        do{
+            System.out.println(n + " ");
+            ++n;
+        }
+        while (n<=10);
+    }
+}
