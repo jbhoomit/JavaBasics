e62a5c75c0e5138041703e54433aad6d5a1c3530
bhoomit jain <bhoonid@gmail.com>
2026-06-15 15:00:57 +0530

    day3 ScannerClass

---
+++ src/ScannerClass.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ScannerClass {
+    public static void main(String[]args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter first num ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter second num");
+        int b = sc.nextInt();
+
+        int sum = a+b;
+
+        System.out.println("sum= "+ sum);
+    }
+}
