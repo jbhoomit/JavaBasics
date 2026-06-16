1098092a50d98ec8e473b407012e548186a922df
bhoomit jain <bhoonid@gmail.com>
2026-06-16 15:29:00 +0530

    day6 Parameter

---
+++ src/ParameterMethod.java
@@ -0,0 +1,8 @@
+public class ParameterMethod {
+    static void greet(String name){
+        System.out.println("hello " + name);
+    }
+    public static void main(String[]args) {
+        greet("bhumit");
+    }
+}
