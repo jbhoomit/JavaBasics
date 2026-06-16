e588af9e467224697aacb950bbe8c33b0c41e619
bhoomit jain <bhoonid@gmail.com>
2026-06-16 15:30:13 +0530

    day6 ReturnType

---
+++ src/ReturnType.java
@@ -0,0 +1,10 @@
+public class ReturnType {
+    static  int mul (int a, int b) {
+        return a * b ;
+    }
+    public static void main(String[]args) {
+        int result = mul (10,60);
+        System.out.println(result);
+    }
+
+}
