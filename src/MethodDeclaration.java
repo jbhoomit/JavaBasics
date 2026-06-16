b436e4198bcf82f7b22a4163ceb6a39caf0bee59
bhoomit jain <bhoonid@gmail.com>
2026-06-16 14:46:43 +0530

    day6 MethodDeclaration

---
+++ src/MethodDeclaration.java
@@ -0,0 +1,9 @@
+public class MethodDeclaration {
+    static void greet() {
+        System.out.println("Hello");
+        System.out.println("bhoomit jain");
+    }
+    public static void main(String[]args) {
+        greet();
+    }
+}
