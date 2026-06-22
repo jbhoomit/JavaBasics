3f6403bff88524a30e2fb7fddd9deb3e8c13870d
bhoomit jain <bhoonid@gmail.com>
2026-06-22 10:05:03 +0530

    day4 arrayTravasal

---
+++ src/ArrayTrevasal.java
@@ -0,0 +1,14 @@
+public class ArrayTrevasal {
+    public static void main(String[]args) {
+        String name [];
+        name =  new String [5];
+        name[0] = "Bhoomit";
+        name[1] = "Tapasya";
+        name[2] = "Priya";
+        name[3] = "Inaya";
+        name[4   ] = "tripti";
+        for (int i = 0; i < name.length; i++) {
+            System.out.println(name[i]);
+        }
+    }
+}
