1ac14c83ce1762ce8f476ec7adc8cb886b234126
bhoomit jain <bhoonid@gmail.com>
2026-06-23 18:36:22 +0530

    day9 Stringbuffer

---
+++ src/StringBuffer.java
@@ -0,0 +1,10 @@
+public class StringBuffer {
+    public static void main(String[] args) {
+
+        java.lang.StringBuffer sb = new java.lang.StringBuffer("Bhoomit");
+
+        sb.append(" Jain");
+
+        System.out.println(sb);
+    }
+}
