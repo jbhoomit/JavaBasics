c65285c00963b22e73fd0f56a91ca77435426ae2
bhoomit jain <bhoonid@gmail.com>
2026-07-03 10:49:45 +0530

    Day12 MultilevelInheritance

---
+++ src/MultiLevelInheretance.java
@@ -0,0 +1,30 @@
+public class MultiLevelInheretance {
+
+        static class Animal {
+            void eat() {
+                System.out.println("Animal is eating");
+            }
+        }
+
+        static class Dog extends Animal {
+            void bark() {
+                System.out.println("Dog is barking");
+            }
+        }
+
+        static class Puppy extends Dog {
+            void weep() {
+                System.out.println("Puppy is weeping");
+            }
+        }
+
+        public static void main(String[] args) {
+
+            Puppy p = new Puppy();
+
+            p.eat();
+            p.bark();
+            p.weep();
+        }
+    }
+
