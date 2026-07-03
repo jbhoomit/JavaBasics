15928f29353e8cafc044dc3c91bf3dc4b51858cd
bhoomit jain <bhoonid@gmail.com>
2026-07-03 10:48:45 +0530

    Day12 SingleInheritance

---
+++ src/SingleInheritance.java
@@ -0,0 +1,22 @@
+public class SingleInheritance {
+
+    static class Animal {
+        void eat() {
+            System.out.println("Animal is eating");
+        }
+    }
+
+    static class Dog extends Animal {
+        void bark() {
+            System.out.println("Dog is barking");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.eat();
+        d.bark();
+    }
+}
