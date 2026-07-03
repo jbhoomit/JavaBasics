8586494633dbdb31e6e595045a1cf8d9da518c5a
bhoomit jain <bhoonid@gmail.com>
2026-07-03 10:51:05 +0530

    Dya12 MethodOverriding

---
+++ src/MethodOverriding.java
@@ -0,0 +1,22 @@
+public class MethodOverriding {
+
+        static class Animal {
+            void sound() {
+                System.out.println("Animal makes sound");
+            }
+        }
+
+        static class Dog extends Animal {
+
+            @Override
+            void sound() {
+                System.out.println("Dog barks");
+            }
+        }
+
+        public static void main(String[] args) {
+
+            Dog d = new Dog();
+            d.sound();
+        }
+    }
