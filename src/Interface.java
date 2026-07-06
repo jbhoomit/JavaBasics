3b9ba0bd34227fa3de3d7b633c809111a664068c
bhoomit jain <bhoonid@gmail.com>
2026-07-06 19:06:11 +0530

    Day14 Interface

---
+++ src/Interface.java
@@ -0,0 +1,14 @@
+public class Interface {
+    interface Animal{
+        void sound();
+    }
+    class Dog implements Animal {
+        public void sound() {
+            System.out.println("Dog barks");
+        }
+    }
+    public void main(String[]args){
+        Dog d = new Dog();
+        d.sound();
+    }
+}
